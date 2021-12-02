5ba91f06b6d03823b570c850cae5101dd1e25fcb
CZH <chancwah@163.com>
2021-12-02 20:41:04 +0800

    新增第7章代码

---
+++ C++PrimerPlus/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    double temp;
+    cin >> temp;
+    if (!cin)
+        cout << "False\n";
+    else
+        cout << "True\n";
+    return 0;
+}
